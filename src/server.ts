59f06303e8f9be940ee80ef2112d0d4d2be2ea75
Pedro-Bittencourt-Costa <pedro.b.costa@academico.unirv.edu.br>
2025-06-22 22:08:48 -0300

    feat: ok

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import { AppDataSource } from "./data_source";
 import  express  from "express";
 import cors from 'cors';
-
+import { equipmentRouter } from "./routes/equipmentRoutes";
+import { loanRouter } from "./routes/loanRoutes";
+import { useRouter } from "./routes/userRoutes";
+import { esportRouter } from "./routes/esportRoutes";
+import { authRouter } from "./routes/authRoutes";
+        
 const app = express();
 
 const PORT = 3000;
@@ -12,6 +17,13 @@ AppDataSource.initialize()
        
         app.use(cors());
         app.use(express.json());
+        app.use('/api/users', useRouter);
+        app.use('/api/loans', loanRouter);
+        app.use('/api/equipments', equipmentRouter); 
+        app.use('/api/esports', esportRouter);
+
+        app.use('/api/auth', authRouter);
+
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
